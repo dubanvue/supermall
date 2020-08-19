25212a6aacf897a919e23da10fba83ceabc2d7ce
dubanvue <173162688@qq.com>
2020-08-19 10:08:01 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h2>hello world</h2>
+    <h2>hello world</h2>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
